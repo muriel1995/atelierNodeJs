82681ea402fb2bbfa167d317fe1a4f6ed3bfbb78
morcillo muriel <morcillomuriel@gmail.com>
2019-03-20 13:59:17 +0100

    Add description README.md

---
+++ wilder.js
@@ -0,0 +1,10 @@
+const chalk = require ('chalk') ; 
+
+console.log (chalk.magenta ('Isaure'));
+console.log(chalk.blue('Walid'));
+console.log(chalk.red('Jean'));
+console.log(chalk.green('Sarah'));
+
+
+
+
